84f1f8a53da470e136520e7f0d2196a8c8577f3d
daeyeon0821 <daeyeon1535@naver.com>
2023-03-06 14:01:26 +0900

    [dylee] Make sample terrain prefab

---
+++ PathFind/Assets/01.UnityProject/Scripts/MapBoard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBoard : MonoBehaviour
+{
+    private const string TERRAIN_MAP_OBJ_NAME = "TerrainMap";
+
+    public Vector2Int MapCellSize { get; private set; } = default;
+    public Vector2 MapCellGap { get; private set; } = default;
+
+}
