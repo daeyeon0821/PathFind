1e5e9300e18e65f4687e1472a541e309a0cb1c41
daeyeon0821 <daeyeon1535@naver.com>
2023-03-07 11:04:13 +0900

    [dylee] Sort terrain tiles ok

--- PathFind/Assets/01.UnityProject/Scripts/PlayScene/TerrainControler.cs
+++ PathFind/Assets/01.UnityProject/Scripts/PlayScene/TerrainControler.cs
@@ -7,6 +7,7 @@ public class TerrainControler : MonoBehaviour
     private const string TILE_FRONT_RENDERER_OBJ_NAME = "FrontRenderer";
 
     private TerrainType terrainType = TerrainType.NONE;
+    private MapBoard mapControler = default;
 
     public bool IsPassable { get; private set; } = false;
     public int TileIdx1D { get; private set; } = -1;
@@ -23,7 +24,63 @@ public class TerrainControler : MonoBehaviour
 
     private void Awake()
     {
-        //frontRenderer = gameObject
+        frontRenderer = gameObject.FindChildComponent<SpriteRenderer>(
+            TILE_FRONT_RENDERER_OBJ_NAME);
+        GFunc.Assert(frontRenderer != null || frontRenderer != default);
+
+        defaultColor = new Color(1f, 1f, 1f, 1f);
+        selectedColor = new Color(236f / 255f, 130f / 255f, 20f / 255f, 1f);
+        searchColor = new Color(0f, 192f / 255f, 0f, 1f);
+        inactiveColor = new Color(128f / 255f, 128f / 255f, 128f / 255f, 1f);
     }       // Awake()
+
+    //! 지형정보를 초기 설정한다.
+    public void SetupTerrain(MapBoard mapControler_, 
+        TerrainType type_, int tileIdx1D_)
+    {
+        terrainType = type_;
+        mapControler = mapControler_;
+        TileIdx1D = tileIdx1D_;
+        TileIdx2D = mapControler.GetTileIdx2D(TileIdx1D);
+
+        string prefabName = string.Empty;
+        switch(type_)
+        {
+            case TerrainType.PLAIN_PASS:
+                prefabName = RDefine.TERRAIN_PREF_PLAIN;
+                IsPassable = true;
+                break;
+            case TerrainType.OCEAN_N_PASS:
+                prefabName = RDefine.TERRAIN_PREF_OCEAN;
+                IsPassable = false;
+                break;
+            default:
+                prefabName = "Tile_Default";
+                IsPassable = false;
+                break;
+        }       // switch: 타일의 타입별로 다른 설정을 한다.
+
+        this.name = string.Format("{0}_{1}", prefabName, TileIdx1D);
+    }       // SetupTerrain()
+
+    //! 지형의 Front 색상을 변경한다.
+    public void SetTileActiveColor(RDefine.TileStatusColor tileStatus)
+    {
+        switch(tileStatus)
+        {
+            case RDefine.TileStatusColor.SELECTED:
+                frontRenderer.color = selectedColor;
+                break;
+            case RDefine.TileStatusColor.SEARCH:
+                frontRenderer.color = searchColor;
+                break;
+            case RDefine.TileStatusColor.INACTIVE:
+                frontRenderer.color = inactiveColor;
+                break;
+            default:
+                frontRenderer.color = defaultColor;
+                break;
+        }
+    }       // SetTileActiveColor()
 
 }       // class TerrainControler
--- PathFind/Assets/01.UnityProject/Scripts/PlayScene/TerrainMap.cs
+++ PathFind/Assets/01.UnityProject/Scripts/PlayScene/TerrainMap.cs
@@ -76,6 +76,31 @@ public class TerrainMap : TileMapControler
         // } 타일맵의 일부를 일정 확률로 다른 타일로 교체하는 로직
 
         // { 기존에 존재하는 타일의 순서를 조정하고, 컨트롤러를 캐싱하는 로직
+        TerrainControler tempTerrain = default;
+        TerrainType terrainType = TerrainType.NONE;
+
+        int loopCnt = 0;
+        foreach (GameObject tile_ in allTileObjs)
+        {
+            tempTerrain = tile_.GetComponentMust<TerrainControler>();
+            switch (tempTerrain.name)
+            {
+                case RDefine.TERRAIN_PREF_PLAIN:
+                    terrainType = TerrainType.PLAIN_PASS;
+                    break;
+                case RDefine.TERRAIN_PREF_OCEAN:
+                    terrainType = TerrainType.OCEAN_N_PASS;
+                    break;
+                default:
+                    terrainType = TerrainType.NONE;
+                    break;
+            }       // switch: 지형별로 다른 설정을 한다.
+
+            tempTerrain.SetupTerrain(mapControler, terrainType, loopCnt);
+            tempTerrain.transform.SetAsFirstSibling();
+            allTerrains.Add(tempTerrain);
+            loopCnt += 1;
+        }       // loop: 타일의 이름과 렌더링 순서대로 정렬하는 루프
         // } 기존에 존재하는 타일의 순서를 조정하고, 컨트롤러를 캐싱하는 로직
 
     }       // Start()
